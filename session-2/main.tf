265f2a3ac95d578862b6f4c8bff7b3c6ed9f0dc3
Mohd Qasim <mohd.qasim@thoughtworks.com>
2024-10-04 12:44:45 +0530

    Add variables mentioned in session-2 exercise

--- session-2/main.tf
+++ session-2/main.tf
@@ -1,28 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.40.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
-variable "region" {
-  type        = string
-  description = "Region to deploy the solution"
-  default     = "ap-south-1"
-}
-
 resource "aws_vpc" "vpc" {
-  cidr_block           = "192.168.1.0/25"
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
   instance_tenancy     = "default"
   tags = {
-    Name = "iac-lab-mq"
+    Name = format("%s-vpc", var.prefix)
   }
 }
