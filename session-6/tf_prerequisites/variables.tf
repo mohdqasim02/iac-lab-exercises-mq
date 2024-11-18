42bf2187e12c8eb41ffeca49e46f49961e009823
Mohd Qasim <mohd.qasim@thoughtworks.com>
2024-11-18 12:55:27 +0530

    [Session-6] Add iam_github.tf

--- session-6/tf_prerequisites/variables.tf
+++ session-6/tf_prerequisites/variables.tf
@@ -4,6 +4,12 @@ variable "prefix" {
   default     = "mq-iac-lab"
 }
 
+variable "repo_name" {
+  type        = string
+  description = "repo name"
+  default     = "mohdqasim02/iac-lab-exercises-mq"
+}
+
 variable "region" {
   type        = string
   description = "Region"
